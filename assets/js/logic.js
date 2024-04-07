acc23813d1b70bf2626379ee043f1530d42fc9fa
frenzie24 <kevincharlesgross@gmail.com>
2024-04-07 16:51:17 -0400

    added header to index.html with link to blog.html and toggle switch. code for toggle switch is attributed to w3schools.com completed first draft of stylings for dark-mode implemented function in logic.js to swap styles for light and dark implemented functions in logic to handle storing and retrieving items. implemented function in logic.js to check the dark-mode toggle value

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,45 @@
 /* Not sure yet */
+
+//function to toggle dark mode
+
+const stores = { toggle: "toggle" };
+
+//  I just dont want to write the same line a million times 
+//  so a wrapper function to set and get is here
+export function setItem(store, val) {
+    localStorage.setItem(store, val);
+}
+
+export function getItem(store) {
+    /*
+     we need to declare a variable here to transform it into 
+    a boolean and return it
+     */
+    let val = localStorage.getItem(store);
+   
+}
+
+/* items are stored as strings
+this parses the stored string back into a bool
+*/
+function checkToggle() {
+    let val = Boolean(getItem("toggle"));
+    return val == null ? true : val;
+}
+
+
+// toggles the style of the first article element in a document between light and dark
+function toggleMode() {
+
+    let article = document.getElementsByTagName("article")[0];
+    var on = checkToggle();
+    console.log(`Current dark-mode status: ${on}`);
+    on = !on;
+    /*
+    if (on === null || on == undefined) { on = true; }
+    else on = !on;
+    */
+   article.className = !on ? "dark-mode" : "gradient-text-coffee";
+    setItem(on);
+}
+
