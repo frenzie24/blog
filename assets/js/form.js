4d2237504a779736b31d6ea2d3ead554fcf729c1
frenzie24 <kevincharlesgross@gmail.com>
2024-04-07 18:44:01 -0400

    finished logic to handle form data gathering added functionality to store a single post for testing '

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,36 @@
 /* js related to controlling our forms */
+
+//import { getItem, setItem } from "./logic";
+//const getItem = require("logic.js").getItem();
+//const setItem= require("logic.js").setItem();
+
+function dataGather(e){
+    e.preventDefault();
+    let data = {};
+    data.userName = document.getElementById("userName").value;
+    data.postTitle = document.getElementById("postTitle").value;
+    data.comment = document.getElementById("comment").value;
+    data.id = Math.floor(Math.random()*1000000);
+    const dataSet = Object.entries(data);
+    let valid = true;
+    dataSet.forEach(([key, val]) => {
+        if (val == '') {
+            alert(`${msg.error.input}`);
+            valid = false;
+            return;
+        }
+    });
+
+    if (valid) {
+        setItem('post', data);
+    } else return;
+    // use a foreach loop to check no data is empty
+
+}
+
+window.onload = () => {
+    const submitBtn = document.querySelector("#post");
+    
+    submitBtn.addEventListener("click", dataGather)
+}
+
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,28 +3,44 @@
 //function to toggle dark mode
 
 const stores = { toggle: "toggle" };
+const msg = {
+    darkStatus: "Current dark-mode status: ",
+    darkChange: "Dark-mode toggle selected.",
+    error: {
+        input: "Error submitting, input invalid.\nPlease make sure all fields have been entered."
+    }
+};
 
+function errHandler()
+{}
 //  I just dont want to write the same line a million times 
 //  so a wrapper function to set and get is here
-export function setItem(store, val) {
+function setItem(store, val) {
+    val = typeof(val) === 'object' ? JSON.stringify(val) : val;
     localStorage.setItem(store, val);
 }
 
-export function getItem(store) {
+function getItem(store) {
     /*
      we need to declare a variable here to transform it into 
     a boolean and return it
      */
     let val = localStorage.getItem(store);
-   
+    return val;
 }
 
 /* items are stored as strings
 this parses the stored string back into a bool
 */
 function checkToggle() {
-    let val = Boolean(getItem("toggle"));
-    return val == null ? true : val;
+    let val = getItem("toggle");
+    /* 
+    This inline conditional forces the return of a boolean.
+    First we check to see if val is null. IF true, return false.
+    ELSE IF val is !null we check if val is string "false". IF true, return false 
+    ELSE IF val is !"false", return true
+    */
+    return val == null ? false : val == "false" ? false : true;
 }
 
 
@@ -33,13 +49,10 @@ function toggleMode() {
 
     let article = document.getElementsByTagName("article")[0];
     var on = checkToggle();
-    console.log(`Current dark-mode status: ${on}`);
+    console.log(`${msg.darkStatus}${on}\n${msg.darkChange}`);
     on = !on;
-    /*
-    if (on === null || on == undefined) { on = true; }
-    else on = !on;
-    */
-   article.className = !on ? "dark-mode" : "gradient-text-coffee";
-    setItem(on);
+    console.log(`${msg.darkStatus}${on}`);
+    article.className = on ? "dark-mode" : "gradient-text-coffee";
+    setItem("toggle", on);
 }
 
