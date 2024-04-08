793885e4b6dca5c8d7b519961f24115bb5b0400e
frenzie24 <kevincharlesgross@gmail.com>
2024-04-08 16:12:58 -0400

    implemented jQuery added some styling to blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,58 +3,65 @@ const postString = {
     userName : "Posted by: ",
     timeStamp: "at ",
 }
-function generateElement(tag, className, id, name, key, val) {
-    var element = document.createElement(tag);
-
+function generateElement(tag, data) {
+    let element = $(`<${tag}>`, data);
+/*
     element.name = name;
     element.className = className;
     element.id = id;
     element.key = key
+    */
     return element;
 }
 
 function generateSimpleElement(tag, val) {
-    var element = document.createElement(tag);
+    let element = document.createElement(tag);
     element.value = val;
     return element;
 }
 
 function generateSimpleTag(tag, val) {
-    var element = document.createElement(tag);
+    let element = document.createElement(tag);
     element.innerText = val;
     return element;
 }
 
 function generateLi(data) {
-    let li = generateElement("li", "post", "post", "post", data.key);
+    let li = generateElement("li", {name:"post", class:"post", id:"post", key: data.key});
     
-    var h3 = generateSimpleTag("h2", data.postTitle);
+    let h3 = generateSimpleTag("h2", data.postTitle);
     
-    var h2 = generateSimpleTag("h3", `${postString.userName}${data.userName}`);
+    let h2 = generateSimpleTag("h3", `${postString.userName}${data.userName}`);
     h2.setAttribute('id', 'username');
-    var p = generateSimpleTag("p", `${data.comment}`);
-    var time = generateSimpleTag("h3", `${postString.timeStamp}${data.timeStamp}`);
+    let p = generateElement("p", {id:'comment'});
+    p.append(data.comment);
+    //   let p = generateElement("p", {id:'comment', innerText: data.comment});
+    // let p = generateSimpleTag("p", `${data.comment}`);
+    let timeStamp = data.timeStamp.split(',');
+    let timeString = "On "
+    let time = generateSimpleTag("h4", `On ${timeStamp[0]}\nAt ${timeStamp[1]}`);
 
-    p.setAttribute('id', 'comment')
-    li.appendChild(h3);
-    li.appendChild(h2);
-    li.appendChild(time);
-    li.appendChild(p);
+   // p.attr('id', 'comment')
+    li.append(h3);
+    li.append(h2);
+    li.append(time);
+    li.append(p);
     return li;
    
 }
 
 function generatePost(data) {
-    var postList = document.getElementById("postList");
-    var post = generateLi(data);
-    postList.appendChild(post);
+    let postList = $("#postList");
+    let post = generateLi(data);
+    postList.append(post);
 
 }
 
 function setup() {
     console.log('setup called')
+    $("#postList").empty();
     let posts = getItem("posts");
-    if (posts.length >0) {
+    if (posts.length > 0) {
        for(let i = 0; i < posts.length; i++) {
             generatePost(posts[i]);
        }
