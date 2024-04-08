19fdaf5d07dfd6ef63d6ac624f4af953939c8cf0
frenzie24 <kevincharlesgross@gmail.com>
2024-04-08 11:31:53 -0400

    added functions to blog.js to handle generating lists

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,46 @@
 /* js related to blog.html */
+
+function generateElement(tag, className, id, name, key, val) {
+    var element = document.createElement(tag);
+
+    element.name = name;
+    element.className = className;
+    element.id = id;
+    element.key = key
+    return element;
+}
+
+function generateElement(tag, val) {
+    var element = document.createElement(tag);
+    element.value = val;
+}
+
+function generateLi(data) {
+    let li = generateElement("li", "post", "", "post", data.key);
+    
+    var h3 = generateElement("h3", data.postTitle);
+    var h2 = generateElement("h2", "");
+    
+    var h2 = generateElement("h2", data.userName);
+    var p = generateElement("p", data.comment);
+    li.appendChild(h3);
+    li.appendChild(h2);
+    li.appendChild(h2);
+    li.appendChild(p);
+    return li;
+   
+}
+
+function generatePost(data) {
+    var postList = document.getElementById("postList");
+    var post = generateLi(data);
+
+}
+
+function setup() {
+    let posts = getItem("posts");
+    if (posts.length >0) {
+        posts.foreach((Object) => {} );
+    }
+
+}
