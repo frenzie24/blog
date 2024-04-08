0813f83d7211c1db4824ee3590bbe9efba3a50e2
frenzie24 <kevincharlesgross@gmail.com>
2024-04-08 16:33:36 -0400

    finished updating existing blog.js to jquery some styling changes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,53 +1,47 @@
 /* js related to blog.html */
 const postString = {
-    userName : "Posted by: ",
-    timeStamp: "at ",
+    userName: "Posted by:",
 }
 function generateElement(tag, data) {
     let element = $(`<${tag}>`, data);
-/*
-    element.name = name;
-    element.className = className;
-    element.id = id;
-    element.key = key
-    */
+    /*
+        element.name = name;
+        element.className = className;
+        element.id = id;
+        element.key = key
+        */
     return element;
 }
 
 function generateSimpleElement(tag, val) {
-    let element = document.createElement(tag);
-    element.value = val;
+    let element = $(`<${tag}>`);
+    element.val(val);
     return element;
 }
 
 function generateSimpleTag(tag, val) {
-    let element = document.createElement(tag);
-    element.innerText = val;
+    let element = $(`<${tag}>`);
+    element.text(val);
     return element;
 }
 
 function generateLi(data) {
-    let li = generateElement("li", {name:"post", class:"post", id:"post", key: data.key});
-    
+    let li = generateElement("li", { name: "post", class: "post", id: "post", key: data.key });
+  
     let h3 = generateSimpleTag("h2", data.postTitle);
-    
-    let h2 = generateSimpleTag("h3", `${postString.userName}${data.userName}`);
-    h2.setAttribute('id', 'username');
-    let p = generateElement("p", {id:'comment'});
-    p.append(data.comment);
-    //   let p = generateElement("p", {id:'comment', innerText: data.comment});
-    // let p = generateSimpleTag("p", `${data.comment}`);
-    let timeStamp = data.timeStamp.split(',');
-    let timeString = "On "
-    let time = generateSimpleTag("h4", `On ${timeStamp[0]}\nAt ${timeStamp[1]}`);
 
-   // p.attr('id', 'comment')
-    li.append(h3);
-    li.append(h2);
-    li.append(time);
-    li.append(p);
+    let h2 = generateElement("h3",{ id: 'comment' });
+    h2.append( `${postString.userName} ${data.userName}`)
+    let p = generateElement("p", { id: 'comment' });
+    p.append(data.comment);
+    let timeStamp = data.timeStamp.split(',');
+    let time = generateSimpleTag("h6", `On ${timeStamp[0]}\nAt ${timeStamp[1]}`);
+
+    // you can append multiple children in one line with jquery
+    li.append( h3,  h2, time, p);
+  
     return li;
-   
+
 }
 
 function generatePost(data) {
@@ -61,12 +55,9 @@ function setup() {
     console.log('setup called')
     $("#postList").empty();
     let posts = getItem("posts");
-    if (posts.length > 0) {
-       for(let i = 0; i < posts.length; i++) {
-            generatePost(posts[i]);
-       }
-    }
-}
-window.onload = () => {
+
+    posts.forEach((post) => {
+        generatePost(post);
+    })
 
 }
